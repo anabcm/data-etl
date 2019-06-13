2601b816cb330ff13b19edc36111d4a27d6879d9
EduardoVasquez <eduardovasquez42@gmail.com>
2019-06-13 15:23:32 -0400

    economic census pipeline

---
+++ etl/inegi_economic_census/pipeline_economic_census.py
@@ -0,0 +1,98 @@
+import pandas as pd
+from bamboo_lib.models import PipelineStep, AdvancedPipelineExecutor
+from bamboo_lib.models import Parameter, BasePipeline
+from bamboo_lib.connectors.models import Connector
+from bamboo_lib.steps import LoadStep
+from bamboo_lib.helpers import grab_connector
+
+class MultiStep(PipelineStep):
+    def run_step(self, prev, params):
+        df = pd.read_csv(
+            "https://storage.googleapis.com/datamexico-data/inegi_economic_census/economic_census.csv", 
+            encoding="latin-1", 
+            low_memory=False)
+
+        df.drop(["Unnamed: 118"], axis=1, inplace=True)
+        
+        temp = list(df.columns[:4])
+        for i in range(4, len(df.columns)):
+            temp.append(df.columns[i].split(" ")[0])
+        df.columns = temp
+
+        df.drop(df.loc[df["Actividad Económica"].str.match("Total nacional") | df["Actividad Económica"].str.match("Total estatal") | df["Actividad Económica"].str.match("Total municipal")].index, inplace=True)
+        df.drop(df[df["Entidad"]=="00 Total Nacional"].index, inplace=True)
+        df = df[~df["Año Censal"].isnull()]
+        df = df[pd.notnull(df["Municipio"])]
+
+        piv_ent = df["Entidad"].str.split(" ", n=1, expand=True)
+        piv_mun = df["Municipio"].str.split(" ", n=1, expand=True)
+        df["Entidad"] = piv_ent[0] + piv_mun[0]
+
+        params = {
+          "Año Censal":"year",
+          "Entidad":"mun_id",
+          "Actividad Económica":"class_id"
+          }
+        df.rename(index=str, columns=params, inplace=True)
+
+        df = df.loc[:, ~df.columns.duplicated()]
+
+        piv_class = df["class_id"].str.split(" ", n=1, expand=True)
+        df["class_id"] = piv_class[0]
+
+        df.drop(["Municipio"], axis=1, inplace=True)
+
+        df["mun_id"] = df["mun_id"].astype(int)
+        df["year"] = df["year"].astype(int)
+
+        df.drop(df.columns[[-1,-2,-3]], axis=1, inplace=True)
+        df.reset_index(drop=True, inplace=True)
+
+        return df
+
+class EconomicCensusPipeline(BasePipeline):
+    @staticmethod
+    def pipeline_id():
+        return "economic-census-pipeline"
+
+    @staticmethod
+    def name():
+        return "Economic Census Pipeline"
+
+    @staticmethod
+    def description():
+        return "Dataframe for Mexican Economic Census"
+
+    @staticmethod
+    def website():
+        return "http://datawheel.us"
+
+    @staticmethod
+    def parameter_list():
+        return [
+            Parameter(label="Source connector", name="source-connector", dtype=str, source=Connector)
+        ]
+
+    @staticmethod
+    def run(params, **kwargs):
+        # Use of connectors specified in the conns.yaml file
+        #source_connector = grab_connector(__file__, params.get("source-connector"))
+        postgres_connector = grab_connector(__file__, "postgres")
+
+        # Definition of each step
+        step1 = MultiStep()
+        step2 = LoadStep("inegi_economic_census", postgres_connector, if_exists="replace", schema = "coverage_schema")
+
+        # Definition of the pipeline and its steps
+        pipeline = AdvancedPipelineExecutor(params)
+        pipeline = pipeline.next(step1).next(step2)
+        return pipeline.run_pipeline()
+
+def run_coverage(params, **kwargs):
+    pipeline = EconomicCensusPipeline()
+    pipeline.run(params)
+
+if __name__ == "__main__":
+    run_coverage({
+        "source-connector": "http-local"
+    })
