d1b44bb51ad80bddca373d9fea896f463fdf72a5
EduardoVasquez <eduardovasquez42@gmail.com>
2019-07-09 17:34:27 -0400

    changes to pipeline 2015 and created pipeline 2010

--- etl/inegi_census/population_pipeline_2010.py
+++ etl/inegi_census/population_pipeline_2010.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from simpledbf import Dbf5 #required given data files
 from bamboo_lib.connectors.models import Connector
 from bamboo_lib.models import EasyPipeline
 from bamboo_lib.models import Parameter
@@ -11,34 +12,36 @@ class TransformStep(PipelineStep):
     def run_step(self, prev, params):
         df_labels = pd.ExcelFile("https://docs.google.com/spreadsheets/d/e/2PACX-1vSYxBmHW5xXkzhVL3X5N21EWWVJKzOkaCfEERaG5lWpmgdx6-Sjcxf7FA7uV1j-_EJIeWZmGbMMDeJh/pub?output=xlsx")
 
-        df = pd.read_csv(prev, dtype=str, index_col=None, header=0, encoding="latin-1")
+        dbf = Dbf5(prev, codec="latin-1")
+        df = dbf.to_dataframe()
         df.columns = df.columns.str.lower()
 
-        # Adding IDs columns and adding year column
+        # Adding ID columns, year and age(edad)
         df["loc_id"] = df["ent"] + df["mun"] + df["loc50k"]
-        df["mun_id_trab"] = df["ent_pais_trab"] + df["mun_trab"]
-        df["year"] = 2015
-
-        # Replacing NaN values with "0" (Not this df.fillna("0", inplace=True)) 
-        # in order to not drop values by GroupBy method
-        df["tie_traslado_trab"].fillna("0", inplace=True)
-        df["med_traslado_trab1"].fillna("0", inplace=True)
-        df["conact"].fillna("0", inplace=True)
-        df["mun_id_trab"].fillna("0", inplace=True)
-
-        # Transforming certains str columns into int values
-        df["loc_id"] = df["loc_id"].astype(int)
-        df["factor"] = df["factor"].astype(int)
-        df["mun_id_trab"] = df["mun_id_trab"].astype(int)
+        df["year"] = 2010
         df["edad"] = df["edad"].astype(int)
+
+        # Adding work places IDs as float type column
+        df["mun_id_trab"] = df["ltrabpai_c"] + df["ltrabmun_c"]
+        df["mun_id_trab"] = df["mun_id_trab"].astype(float)
 
         # Turning work places IDs to 0, which are overseas
         df.loc[df["mun_id_trab"] > 33000, "mun_id_trab"] = 0
         df["mun_id_trab"].replace(pd.np.nan , 0, inplace=True)
 
+        # Adding columns related to work, which are not in the original DF (Exist in the 2015 intercensal census data)
+        df["laboral_condition"] = ""
+        df["time_to_work"] = ""
+        df["transport_mean_work"] = ""
+
+        # Transforming certains str columns into int values
+        df["loc_id"] = df["loc_id"].astype(int)
+        df["factor"] = df["factor"].astype(int)
+
         # List of columns for the next df
-        params            = ["sexo", "parent", "sersalud", "dhsersal1", "conact", "tie_traslado_trab", "med_traslado_trab1"]
-        params_translated = ["sex", "parent", "sersalud", "dhsersal1", "laboral_condition", "time_to_work", "transport_mean_work"]
+        params = ["sexo", "parent", "sersalud", "dhsersal1"]
+        params_nan = ["laboral_condition", "time_to_work", "transport_mean_work"]
+        params_int = ["loc_id", "year", "mun_id_trab"]
 
         # For cycle in order to change the content of a column from previous id, into the new ones (working for translate too)
         for sheet in params:
@@ -46,28 +49,28 @@ class TransformStep(PipelineStep):
             df[sheet] = df[sheet].astype(int)
             df[sheet] = df[sheet].replace(dict(zip(df_l.prev_id, df_l.id)))
 
-        # Renaming of certains columns (Nacionality is not added given 2010 data, for now)
-        df.rename(index=str, columns={
-                            "factor": "population",
-                            "sexo": "sex", 
-                            "conact": "laboral_condition", 
-                            "tie_traslado_trab": "time_to_work",
-                            "med_traslado_trab1": "transport_mean_work"}, inplace=True)
 
         # Condense df around params list, mun_id and loc_id, and sum over population (factor)
-        df = df.groupby(params_translated + ["loc_id", "mun_id_trab", "age"]).sum().reset_index(col_fill="ffill")
+        df = df.groupby(params + params_nan + params_int + ["edad"]).sum().reset_index(col_fill="ffill")
 
-        # Turning back NaN values
-        df["time_to_work"].replace(0, pd.np.nan, inplace=True)
-        df["transport_mean_work"].replace(0, pd.np.nan, inplace=True)
-        df["laboral_condition"].replace(0, pd.np.nan, inplace=True)
+        # Filling empty values with NaN, an replacing 0 values in mun_id_trab
+        df["laboral_condition"] = pd.np.nan
+        df["time_to_work"] = pd.np.nan
+        df["transport_mean_work"] = pd.np.nan
         df["mun_id_trab"].replace(0, pd.np.nan, inplace=True)
+
+
+        # Renaming of certains columns
+        df.rename(index=str, columns={
+                                    "factor": "population",
+                                    "edad": "age",
+                                    "sexo": "sex"}, inplace=True)
 
         # Not answered age values, turned to text (Column as object type)
         df["age"].replace(999, "Edad no especificada", inplace=True)
 
-        # Transforming certains columns to objects
-        for col in (params_translated + ["mun_id_trab"]):
+        # Transforming certains columns into int values
+        for col in ["sex", "parent", "sersalud", "dhsersal1", "laboral_condition", "time_to_work", "transport_mean_work", "mun_id_trab", "age"]:
             df[col] = df[col].astype("object")
 
         return df
@@ -94,8 +97,7 @@ class PopulationPipeline(EasyPipeline):
             "time_to_work":        "UInt8",
             "transport_mean_work": "UInt8",
             "mun_id_trab":         "UInt8",
-            "age":                 "UInt8",
-            "year":                "UInt8",
+            "age":                 "UInt8"
         }
 
         download_step = DownloadStep(
@@ -105,8 +107,9 @@ class PopulationPipeline(EasyPipeline):
         transform_step = TransformStep()
         load_step = LoadStep(
             "inegi_population", db_connector, if_exists="append", pk=["loc_id", "sex"], dtype=dtype, 
-            nullable_list=[
-                "time_to_work", "transport_mean_work", "laboral_condition", "mun_id_trab"]
+            nullable_list=["parent", "laboral_condition", 
+                          "time_to_work", "transport_mean_work", 
+                          "sersalud", "dhsersal1"]
         )
 
         return [download_step, transform_step, load_step]
