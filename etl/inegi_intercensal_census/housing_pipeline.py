90c04b7cbfd42285049adfc3ecdb92f959bed0b8
D2SD <samueloso21@gmail.com>
2019-07-11 11:32:29 -0400

    change groupby transformation columns format

--- etl/inegi_intercensal_census/housing_pipeline.py
+++ etl/inegi_intercensal_census/housing_pipeline.py
@@ -36,29 +36,30 @@ class TransformStep(PipelineStep):
         # replace, select and group data
         for col in data.keys():
             df[col] = df[col].replace(dict(zip(data[col]['prev_id'], data[col]['id'].astype('int'))))
+
+        # income interval replace
+        url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR08Js9Sh4nNTMe5uBcsDUFedG5MOjIf90p6EHAr1_sWY5kpnI3xUvyPHzQpTEUrXz1pskaoc0uyea6/pub?output=xlsx'
+        income = pd.read_excel(url, sheet_name='income', encoding='latin-1')
+        for ing in df.ingtrhog.unique():
+            for level in range(income.shape[0]):
+                if (ing >= income.interval_lower[level]) & (ing < income.interval_upper[level]):
+                    df.ingtrhog = df.ingtrhog.replace(ing, income.id[level])
+                    break
+                if ing >= income.interval_upper[income.shape[0]-1]:
+                    df.ingtrhog = df.ingtrhog.replace(ing, income.id[income.shape[0]-1])
+                    break
+
+        # subset of columns
         df = df[['loc_id', 'cobertura', 'ingtrhog', 'pisos', 'techos', 'paredes', 'forma_adqui', 'deuda', 'factor', 'numpers', 'financiamiento', 'totcuart', 'cuadorm', 'clavivp', 'ingr_ayugob', 'ingr_perotropais']].copy(deep=True)
         df.fillna('temp', inplace=True)
         df = df.groupby(['loc_id', 'factor', 'cobertura', 'clavivp', 'forma_adqui', 'financiamiento', 'deuda', 'ingr_ayugob', 'ingr_perotropais', 'ingtrhog', 'pisos', 'techos', 'paredes', 'numpers', 'totcuart', 'cuadorm']).sum().reset_index(col_fill='ffill')
         df = df.rename(columns={'factor': 'inhabitants', 'pisos': 'floor', 'paredes': 'wall', 'techos': 'roof', 'forma_adqui': 'acquisition', 'deuda': 'debt', 'ingtrhog': 'income', 'cobertura': 'coverage', 'clavivp': 'home_type', 'financiamiento': 'funding', 'ingr_ayugob': 'government_financial_aid', 'ingr_perotropais': 'foreign_financial_aid', 'numpers': 'n_inhabitants', 'totcuart': 'total_rooms', 'cuadorm': 'bedrooms'})
         df.replace('temp', pd.np.nan, inplace=True)
-        return df
 
-class IncomeIntervalStep(PipelineStep):
-    def run_step(self, prev, params):
-        df = prev
-        # income interval
-        url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR08Js9Sh4nNTMe5uBcsDUFedG5MOjIf90p6EHAr1_sWY5kpnI3xUvyPHzQpTEUrXz1pskaoc0uyea6/pub?output=xlsx'
-        income = pd.read_excel(url, sheet_name='income', encoding='latin-1')
-        for ing in df.income.unique():
-            for level in range(income.shape[0]):
-                if (ing >= income.interval_lower[level]) & (ing < income.interval_upper[level]):
-                    df.income = df.income.replace(ing, income.id[level])
-                    break
-                if ing >= income.interval_upper[income.shape[0]-1]:
-                    df.income = df.income.replace(ing, income.id[income.shape[0]-1])
-                    break
+        # data types
         for col in df.columns:
             df[col] = df[col].astype('object')
+
         return df
 
 class CoveragePipeline(EasyPipeline):
@@ -116,7 +117,6 @@ class CoveragePipeline(EasyPipeline):
         read_step = ReadStep()
         clean_step = CleanStep()
         transform_step = TransformStep()
-        income_transform_step = IncomeIntervalStep()
 
         load_step = LoadStep(
             'inegi_housing', db_connector, if_exists='append', pk=['loc_id'], dtype=dtype, 
@@ -125,4 +125,4 @@ class CoveragePipeline(EasyPipeline):
                           'n_inhabitants', 'total_rooms', 'bedrooms']
         )
 
-        return [download_step, read_step, clean_step, transform_step, income_transform_step, load_step]
+        return [download_step, read_step, clean_step, transform_step, load_step]
