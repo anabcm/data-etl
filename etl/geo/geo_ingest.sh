c8fc3e9ed5612bedc38ae156d1848e59ad9d0efc
cnavarreteliz <cnavarreteliz@gmail.com>
2019-07-19 15:07:26 -0400

    creates ingestion scripts for shared dimensions

--- etl/geo/geo_ingest.sh
+++ etl/geo/geo_ingest.sh
@@ -1,3 +1,5 @@
 bamboo-cli --folder . --entry dim_loc_pipeline
 bamboo-cli --folder . --entry dim_mun_pipeline
 bamboo-cli --folder . --entry dim_ent_pipeline
+bamboo-cli --folder . --entry dim_zm_pipeline
+bamboo-cli --folder . --entry dim_sun_pipeline
