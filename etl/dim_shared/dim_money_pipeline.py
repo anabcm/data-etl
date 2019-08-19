fcb31ac14b1ac847b67d2c1b830b373dd0158f43
cnavarreteliz <cnavarreteliz@gmail.com>
2019-08-19 11:42:38 -0400

    updates dim shared for monetary drilldowns

--- etl/dim_shared/dim_money_pipeline.py
+++ etl/dim_shared/dim_money_pipeline.py
@@ -7,12 +7,16 @@ from bamboo_lib.steps import LoadStep
 class ReadStep(PipelineStep):
     def run_step(self, prev, params):
         # income range data
-        url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR08Js9Sh4nNTMe5uBcsDUFedG5MOjIf90p6EHAr1_sWY5kpnI3xUvyPHzQpTEUrXz1pskaoc0uyea6/pub?output=xlsx'
-        df = pd.read_excel(url, dtype='str', sheet_name='income', usecols=[0,1,4])
-        df.id = df.id.astype('int')
+        url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTieVnRovfP7AOMtqxIJcrFl8Tayz6Irz-Bc1en1NSIKtjjPtGaBRaCaSeePRrpQMmHMzSt2VO93Wav/pub?output=csv'
+        df = pd.read_csv(url, dtype='str')
+
+        df['id'] = df.id.astype(int)
+        df['enigh_group_id'] = df['enigh_group_id'].astype(int)
+
+        df = df[['id', 'enigh_group_id', 'enigh_group', 'name_en', 'name_es']]
         return df
 
-class CoveragePipeline(EasyPipeline):
+class DimMoneyPipeline(EasyPipeline):
         @staticmethod
         def description():
             return 'ETL Income dim table'
@@ -22,15 +26,17 @@ class CoveragePipeline(EasyPipeline):
             # Use of connectors specified in the conns.yaml file
             db_connector = Connector.fetch('clickhouse-database', open('../conns.yaml'))
             dtype = {
-                'id':      'UInt8',
-                'name_en': 'String',
-                'name_es': 'String',
+                'id':               'UInt8',
+                'enigh_group_id':   'UInt8'
+                'enigh_group':      'String',
+                'name_en':          'String',
+                'name_es':          'String',
             }
     
             read_step = ReadStep()
     
             load_step = LoadStep(
-                'dim_shared_income', db_connector, if_exists='drop', pk=['id'], dtype=dtype
+                'dim_shared_money', db_connector, if_exists='drop', pk=['id'], dtype=dtype
             )
     
             return [read_step, load_step]
