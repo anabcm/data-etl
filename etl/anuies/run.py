d454681f3f09eb534177cd6b6125c9179c012b3c
D2SD <samueloso21@gmail.com>
2019-08-25 19:17:28 -0400

    updates work centers ids

--- etl/anuies/run.py
+++ etl/anuies/run.py
@@ -1,11 +1,11 @@
 import os
 
 data = ['https://storage.googleapis.com/datamexico-data/anuies/postgraduate/posgrado_2016-2017.xlsx',
-      'https://storage.googleapis.com/datamexico-data/anuies/postgraduate/posgrado_2017-2018.xlsx']
+        'https://storage.googleapis.com/datamexico-data/anuies/postgraduate/posgrado_2017-2018.xlsx']
 
 cmd = ['bamboo-cli --folder . --entry enrollment_pipeline', 
-      'bamboo-cli --folder . --entry origin_pipeline', 
-      'bamboo-cli --folder . --entry status_pipeline']
+       'bamboo-cli --folder . --entry origin_pipeline', 
+       'bamboo-cli --folder . --entry status_pipeline']
 
 # data
 for url in data:
@@ -13,8 +13,10 @@ for url in data:
     os.system(command + ' --url=' + url)
 
 # work center index
-for ent in range(1,33):
-  os.system('bamboo-cli --folder . --entry work_centers_pipeline --index=' + str(ent))
+data = ['https://storage.googleapis.com/datamexico-data/anuies/work_center/index_work_center_posgrado_2016-2017.xlsx',
+        'https://storage.googleapis.com/datamexico-data/anuies/work_center/index_work_center_posgrado_2017-2018.xlsx']
+for url in data:
+  os.system('bamboo-cli --folder . --entry universities_index_pipeline --url=' + url)
 
 # careers index
 os.system('bamboo-cli --folder . --entry careers_programs')
