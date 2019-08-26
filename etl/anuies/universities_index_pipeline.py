37f72fca5fbef0c2885a52c9c506ca597e793c0c
D2SD <samueloso21@gmail.com>
2019-08-26 17:10:50 -0400

    replace index campus values

--- etl/anuies/universities_index_pipeline.py
+++ etl/anuies/universities_index_pipeline.py
@@ -2,7 +2,7 @@ def format_text(df, cols_names=None, stopwords=None):
 
     # format
     for ele in cols_names:
-        df[ele] = df[ele].str.title()
+        df[ele] = df[ele].str.title().str.strip()
         for ene in stopwords:
             df[ele] = df[ele].str.replace(' ' + ene.title() + ' ', ' ' + ene + ' ')
 
@@ -21,12 +21,13 @@ class ReadStep(PipelineStep):
         # external ids
         url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTzv8dN6-Cn7vR_v9UO5aPOBqumAy_dXlcnVOFBzxCm0C3EOO4ahT5FdIOyrtcC7p-akGWC_MELKTcM/pub?output=xlsx'
         ent = pd.read_excel(url, sheet_name='origin', dtypes='str')
+        camp = pd.read_excel(url, sheet_name='campus', dtypes='str')
 
-        return df, ent
+        return df, ent, camp
 
 class TransformStep(PipelineStep):
     def run_step(self, prev, params):
-        df, ent = prev[0], prev[1]
+        df, ent, camp = prev[0], prev[1], prev[2]
         # type format
         for col in ['mun_id', 'ent_id']:
             df[col] = df[col].ffill()
@@ -38,25 +39,27 @@ class TransformStep(PipelineStep):
         }
         df.sostenimiento.replace(sostenimiento, inplace=True)
         
+        # stopwords
+        stopwords_es = ['a', 'e', 'ante', 'con', 'contra', 'de', 'desde', 'la', 'lo', 'las', 'los', 'y']
+
+        cols_es = ['work_center_name', 'campus']
+
+        #format
+        df = format_text(df, cols_names=cols_es, stopwords=stopwords_es)
+
         # ids replace from external table
         df.ent_id = df.ent_id.str.title()
         df.ent_id.replace(dict(zip(ent.origin, ent.id)), inplace=True)
+        df.campus = df.campus.str.replace(',', '').str.replace('  ', ' ')
+        df.campus.replace(dict(zip(camp.name, camp.id)), inplace=True)
         
         # minicipality id
         df.mun_id = df.mun_id.astype('int').astype('str').str.zfill(3)
         df.loc[:, 'mun_id'] = df.ent_id.astype('str') + df.mun_id
         df.drop(columns='ent_id', inplace=True)
 
-        # stopwords
-        stopwords_es = ['a', 'e', 'ante', 'con', 'contra', 'de', 'desde', 'la', 'lo', 'las', 'los', 'y']
-
-        cols_es = ['work_center_name']
-
-        #format
-        df = format_text(df, cols_names=cols_es, stopwords=stopwords_es)
-
         # type conversion
-        for col in ['mun_id', 'sostenimiento']:
+        for col in ['mun_id', 'sostenimiento', 'campus']:
             df[col] = df[col].astype('float')
         
         return df
@@ -77,7 +80,7 @@ class EnrollmentPipeline(EasyPipeline):
             'mun_id':           'UInt16',
             'work_center_id':   'String',
             'work_center_name': 'String',
-            'campus':           'String',
+            'campus':           'UInt16',
             'sostenimiento':    'UInt8'
         }
         
