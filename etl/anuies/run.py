f7b1a1a71dfe592b3ca538015ee194e83e088b2b
D2SD <samueloso21@gmail.com>
2019-08-28 10:28:49 -0400

    uncomment code

--- etl/anuies/run.py
+++ etl/anuies/run.py
@@ -1,6 +1,6 @@
 import os
 
-"""data = ['https://storage.googleapis.com/datamexico-data/anuies/temp/posgrado_2017-2018.xlsx',
+data = ['https://storage.googleapis.com/datamexico-data/anuies/temp/posgrado_2017-2018.xlsx',
         'https://storage.googleapis.com/datamexico-data/anuies/temp/posgrado_2016-2017.xlsx',
         'https://storage.googleapis.com/datamexico-data/anuies/temp/licenciatura_2017-2018.xlsx',
         'https://storage.googleapis.com/datamexico-data/anuies/temp/licenciatura_2016-2017.xlsx']
@@ -12,13 +12,13 @@ cmd = ['bamboo-cli --folder . --entry enrollment_pipeline',
 # data
 for url in data:
   for command in cmd:
-    os.system(command + ' --url=' + url)"""
+    os.system(command + ' --url=' + url)
 
 # work center index
 os.system('bamboo-cli --folder . --entry universities_index_pipeline')
 
-"""# careers index
+# careers index
 os.system('bamboo-cli --folder . --entry careers_index_programs')
 
 # campus index
-os.system('bamboo-cli --folder . --entry campus_index_pipeline')"""
+os.system('bamboo-cli --folder . --entry campus_index_pipeline')
