f576ec687c43484c39f29a2ba263cab91111baf9
EduardoVasquez <eduardovasquez42@gmail.com>
2019-09-03 15:22:22 -0400

    cie10 pipeline and reorder PM elements

---
+++ etl/shared/dim_cie10_pipeline.py
@@ -0,0 +1,75 @@
+import pandas as pd
+from bamboo_lib.models import PipelineStep
+from bamboo_lib.models import EasyPipeline
+from bamboo_lib.connectors.models import Connector
+from bamboo_lib.steps import LoadStep
+# English stopwords
+from sklearn.feature_extraction import stop_words
+
+class ReadStep(PipelineStep):
+    def run_step(self, prev, params):
+        url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSBBIm3kROrey05M9VoRDvGM7uWIQis-Xx1Zy4x1zVvuGPv39JFAcW6Vi7kRW2PgInjRgNecEnxXi5T/pub?output=xlsx"
+        df = pd.read_excel(url, dtype="str")
+        return df
+
+class CleanStep(PipelineStep):
+    def run_step(self, prev, params):
+        df = prev
+
+        # Setting column names to set in format
+        cols_es = ["categoria_letra_es", "cie10_3_es", "cie10_4_es"]
+        cols_en = ["categoria_letra_en", "cie10_3_en", "cie10_4_en"]
+        stopwords_es = ["a", "ante", "con", "contra", "de", "desde", "la", "lo", "las", "los", "y"]
+
+        # Step for spanish words
+        for ele in cols_es:
+            df[ele] = df[ele].str.title()
+            for ene in stopwords_es:
+                df[ele] = df[ele].str.replace(" " + ene.title() + " ", " " + ene + " ")
+
+        # Step for english words
+        for ele in cols_en:
+            df[ele] = df[ele].str.title()
+            for ene in list(stop_words.ENGLISH_STOP_WORDS):
+                df[ele] = df[ele].str.replace(" " + ene.title() + " ", " " + ene + " ")
+
+        # Groupby step
+        grouped = ["letra_id", "categoria_letra_es", "categoria_letra_en", "cie10_3_digitos", "cie10_3_es", "cie10_3_en",
+           "cie10_4_digitos", "cie10_4_es", "cie10_4_en"]
+
+        df = df.groupby(grouped).sum().reset_index(col_fill="ffill")
+
+        return df
+
+class CoveragePipeline(EasyPipeline):
+    @staticmethod
+    def description():
+        return "Processes IDC codes from Mexico"
+
+    @staticmethod
+    def website():
+        return "http://datawheel.us"
+
+    @staticmethod
+    def steps(params, **kwargs):
+        # Use of connectors specified in the conns.yaml file
+        db_connector = Connector.fetch("clickhouse-database", open("../conns.yaml"))
+        dtype = {
+
+            "letra_id":                "String",
+            "categoria_letra_es":      "String",
+            "categoria_letra_en":      "String",
+            "cie10_3_digitos":         "String",
+            "cie10_3_es":              "String",
+            "cie10_3_en":              "String",
+            "cie10_4_digitos":         "String",
+            "cie10_4_es":              "String",
+            "cie10_4_en":              "String"
+        }
+
+        # Definition of each step
+        read_step = ReadStep()
+        clean_step = CleanStep()
+        load_step = LoadStep("dim_shared_cie10", db_connector, if_exists="drop", pk=["letra_id", "cie10_3_digitos", "cie10_4_digitos"], dtype=dtype)
+        
+        return [read_step, clean_step, load_step]
