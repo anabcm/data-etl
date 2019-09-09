5f60ac915928149b415a3db4cd04fede802f075d
EduardoVasquez <eduardovasquez42@gmail.com>
2019-09-08 21:27:23 -0300

    general and 1 years old mortality pipe.

--- etl/healthcare/mortality/mortality_1year_pipeline.py
+++ etl/healthcare/mortality/mortality_1year_pipeline.py
@@ -17,8 +17,8 @@ class TransformStep(PipelineStep):
         _years = ["1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005",
                   "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016"]
 
-        # Keeping columns related to totals, not percentage and ratios rows
-        df = df[df["unidad_medida"] == "Defunciones"]
+        # Division per gender (1 year olds)
+        df = df[(df["id_indicador"] == 1002000035) | (df["id_indicador"] == 1002000036)| (df["id_indicador"] == 1002000037)]
 
         # Droping rows related to "Estados Unidos Mexicanos" or national/entity/municipality totals
         df.drop(df.loc[(df["entidad"] == 0) | (df["municipio"] == 0) ].index, inplace=True)
@@ -30,17 +30,20 @@ class TransformStep(PipelineStep):
         df.drop(["entidad", "municipio", "desc_entidad", "desc_municipio", "indicador", "unidad_medida",
                 "1990", "1991", "1992", "1993"], axis=1, inplace=True)
 
+        # Division per gender (totals) [1: male, 2: female, 0: unknownj]
+        df["id_indicador"].replace({1002000035: 1, 1002000036: 2, 1002000037: 0}, inplace=True)
+
         # Melt step in order to get file in tidy data format
         df = pd.melt(df, id_vars = ["mun_id", "id_indicador"], value_vars = _years)
 
         # Renaming columns from spanish to english
-        df.rename(columns = {"id_indicador": "description_id", "variable": "year", "value": "count"}, inplace=True)
+        df.rename(columns = {"id_indicador": "gender_id", "variable": "year", "value": "count"}, inplace=True)
 
         # Groupby step, in order to set count values from NaN to 0
-        df = df.groupby(["mun_id", "description_id", "year"]).sum().reset_index(col_fill="ffill")
+        df = df.groupby(["mun_id", "gender_id", "year"]).sum().reset_index(col_fill="ffill")
 
         # Setting types
-        for item in ["mun_id", "description_id", "year", "count"]:
+        for item in ["mun_id", "gender_id", "year", "count"]:
             df[item] = df[item].astype(int)
 
         return df
@@ -56,7 +59,7 @@ class ENOEPipeline(EasyPipeline):
 
         dtype = {
             "mun_id":                   "UInt16",
-            "description_id":           "UInt32",
+            "gender_id":                "UInt8",
             "year":                     "UInt16",
             "count":                    "UInt16",
         }
@@ -67,7 +70,7 @@ class ENOEPipeline(EasyPipeline):
         )
         transform_step = TransformStep()
         load_step = LoadStep(
-            "inegi_mortality", db_connector, if_exists="append", pk=["mun_id", "description_id", "year"], dtype=dtype
+            "inegi_1years_mortality", db_connector, if_exists="append", pk=["mun_id", "gender_id", "year"], dtype=dtype
         )
 
         return [download_step, transform_step, load_step]
