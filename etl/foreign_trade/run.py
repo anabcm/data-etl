c936fefdda200905ec7706044ae4c9f58c6ff3df
D2SD <samueloso21@gmail.com>
2019-09-10 09:54:57 -0300

    dimension table

--- etl/foreign_trade/run.py
+++ etl/foreign_trade/run.py
@@ -9,5 +9,8 @@ for year in range(6, 19):
       url = 'https://storage.googleapis.com/datamexico-data/foreign_trade/Municipal/HS_6D/Monthly/6D_mun_monthly' + str(month).zfill(2) + str(year).zfill(2) + '.csv'
       os.system('bamboo-cli --folder . --entry foreign_trade_pipeline --url=' + url)
 
-# campus index
+# countries
 os.system('bamboo-cli --folder . --entry countries_ingest')
+
+# hs6 2012
+os.system('bamboo-cli --folder . --entry hs_codes_ingest')
