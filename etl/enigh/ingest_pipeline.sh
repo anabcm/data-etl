f3b6aca8f6aafc6ed553556f71d3900730e7d3df
EduardoVasquez <eduardovasquez42@gmail.com>
2019-09-27 13:06:18 -0300

    sh file update

--- etl/enigh/ingest_pipeline.sh
+++ etl/enigh/ingest_pipeline.sh
@@ -1,2 +1,11 @@
 bamboo-cli --folder . --entry enigh_jobs_pipeline
 bamboo-cli --folder . --entry enigh_population_pipeline
+bamboo-cli --folder . --entry enigh_population_pipeline
+
+for i in $(seq -f "%02g" 2016 2018)
+do
+    bamboo-cli --folder . --entry enigh_jobs_pipeline --year="$i"
+    bamboo-cli --folder . --entry enigh_population_pipeline --year="$i"
+    bamboo-cli --folder . --entry enigh_population_pipeline --year="$i"
+
+done
