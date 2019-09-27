2c0e605a404f12c2d6e962041a5aa01bbcfaa8e8
EduardoVasquez <eduardovasquez42@gmail.com>
2019-09-27 13:06:40 -0300

    deleted unused texts

--- etl/enigh/ingest_pipeline.sh
+++ etl/enigh/ingest_pipeline.sh
@@ -1,7 +1,3 @@
-bamboo-cli --folder . --entry enigh_jobs_pipeline
-bamboo-cli --folder . --entry enigh_population_pipeline
-bamboo-cli --folder . --entry enigh_population_pipeline
-
 for i in $(seq -f "%02g" 2016 2018)
 do
     bamboo-cli --folder . --entry enigh_jobs_pipeline --year="$i"
