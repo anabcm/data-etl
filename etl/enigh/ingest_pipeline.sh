fec432311376858621afa22e6dfde8196f11126c
D2SD <samueloso21@gmail.com>
2019-09-30 11:15:49 -0300

    add pipeline

--- etl/enigh/ingest_pipeline.sh
+++ etl/enigh/ingest_pipeline.sh
@@ -2,6 +2,6 @@ for i in $(seq -f "%02g" 2016 2018)
 do
     bamboo-cli --folder . --entry enigh_jobs_pipeline --year="$i"
     bamboo-cli --folder . --entry enigh_population_pipeline --year="$i"
-    bamboo-cli --folder . --entry enigh_population_pipeline --year="$i"
+    bamboo-cli --folder . --entry enigh_household_income_pipeline --year="$i"
 
 done
