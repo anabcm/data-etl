126eda2804ecd8d514e3e0dfba6813fbefb72364
D2SD <samueloso21@gmail.com>
2020-01-08 10:33:53 -0300

    gini coneval nat level

--- etl/coneval/coneval_ingest.sh
+++ etl/coneval/coneval_ingest.sh
@@ -4,3 +4,4 @@ bamboo-cli --folder . --entry social_lag_pipeline_mun
 bamboo-cli --folder . --entry social_lag_pipeline_ent
 bamboo-cli --folder . --entry gini_pipeline_mun
 bamboo-cli --folder . --entry gini_pipeline_ent
+bamboo-cli --folder . --entry gini_pipeline_nat
---
+++ etl/coneval/gini_pipeline_nat.py
@@ -0,0 +1,50 @@
+
+import pandas as pd
+from bamboo_lib.connectors.models import Connector
+from bamboo_lib.models import EasyPipeline
+from bamboo_lib.models import PipelineStep
+from bamboo_lib.steps import DownloadStep
+from bamboo_lib.steps import LoadStep
+
+class TransformStep(PipelineStep):
+    def run_step(self, prev, params):
+        excel = pd.ExcelFile('https://storage.googleapis.com/datamexico-data/coneval/GINI_Ent.xlsx')
+
+        sheets = {'Coeficiente de Gini 2008-2014': ['ent_id', '2008', '2010', '2012', '2014'],
+                'Coeficiente de Gini 2016-2018': ['ent_id', '2016', '2018']}
+        df = pd.DataFrame()
+        df_temp = pd.DataFrame()
+
+        for sheet, cols in sheets.items():
+            df_temp = pd.read_excel(excel, sheet_name=sheet, header=6)
+            df_temp.dropna(axis=1, how='all', inplace=True)
+            df_temp.dropna(axis=0, how='any', inplace=True)
+            df_temp.columns = cols
+            df_temp = df_temp.melt(id_vars='ent_id', var_name='year', value_name='gini').copy()
+            df_temp = df_temp.loc[df_temp['ent_id'] == 'Estados Unidos Mexicanos'].copy()
+            url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSDaqIIMI56NCwzU1fJxz6erC474xtqJBytaBaqVJS6b5Op7nr1p_sE1Fq4XKVaNdDjoz-yOzX1rRj6/pub?output=xlsx'
+            df_temp['ent_id'].replace({'Estados Unidos Mexicanos': 0}, inplace=True)
+            df = df.append(df_temp)
+
+        df['ent_id'] = df['ent_id'].astype('int')
+        df['year'] = df['year'].astype('int')
+        df['gini'] = df['gini'].astype('float')
+
+        return df
+
+class CONEVALGiniPipeline(EasyPipeline):
+    @staticmethod
+    def steps(params):
+        db_connector = Connector.fetch("clickhouse-database", open("../conns.yaml"))
+        dtype = {
+            "gini":         "Float32",
+            "ent_id":       "UInt8",
+            "year":         "UInt16"
+        }
+
+        transform_step = TransformStep()
+        load_step = LoadStep(
+            "coneval_gini_nat", db_connector, if_exists="drop", pk=["ent_id", "year"], dtype=dtype
+        )
+
+        return [transform_step, load_step]
