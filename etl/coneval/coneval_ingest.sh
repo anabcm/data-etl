dea7b8300dc5d88dbeb8e7cdbbd9a532a4744b35
D2SD <samueloso21@gmail.com>
2020-01-08 10:30:21 -0300

    coneval gini state level

--- etl/coneval/coneval_ingest.sh
+++ etl/coneval/coneval_ingest.sh
@@ -3,3 +3,4 @@ bamboo-cli --folder . --entry poverty_pipeline --year="2015"
 bamboo-cli --folder . --entry social_lag_pipeline_mun
 bamboo-cli --folder . --entry social_lag_pipeline_ent
 bamboo-cli --folder . --entry gini_pipeline_mun
+bamboo-cli --folder . --entry gini_pipeline_ent
