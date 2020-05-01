fc8deddd0c4dfcd029a597b5e2123f9497fce36d
D2SD <samueloso21@gmail.com>
2020-05-01 11:16:51 -0400

    refactor run covid

--- etl/covid/run.py
+++ etl/covid/run.py
@@ -1,10 +1,15 @@
 
 import os
 import pandas as pd
+from covid_pipeline import CovidPipeline
+from dim_time_date import DimTimeDatePipeline
 from bamboo_lib.helpers import query_to_df
 from bamboo_lib.connectors.models import Connector
 
-os.system('bamboo-cli --folder . --entry covid_pipeline')
+#os.system('bamboo-cli --folder . --entry covid_pipeline')
+
+pp = CovidPipeline()
+pp.run({})
 
 db_connector = Connector.fetch('clickhouse-database', open('../conns.yaml'))
 min_query = 'SELECT min(ingress_date), min(symptoms_date), min(death_date), min(updated_date) FROM gobmx_covid'
@@ -13,4 +18,8 @@ min_value = str(min(query_to_df(db_connector, raw_query=min_query).iloc[0].to_li
 max_value = str(max(query_to_df(db_connector, raw_query=max_query).iloc[0].to_list()))
 min_time = '{}-{}-{}'.format(min_value[:4], min_value[4:6], min_value[6:])
 max_time = '{}-{}-{}'.format(max_value[:4], max_value[4:6], max_value[6:])
-os.system('bamboo-cli --folder . --entry dim_time_date --init={} --end={}'.format(min_time, max_time))
+#os.system('bamboo-cli --folder . --entry dim_time_date --init={} --end={}'.format(min_time, max_time))
+
+pp = DimTimeDatePipeline()
+pp.run({'init': min_time,
+        'end': max_time})
