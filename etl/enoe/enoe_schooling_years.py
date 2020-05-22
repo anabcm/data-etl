2a0c318a804913f7121ed08fcebddd80af7b8295
D2SD <samueloso21@gmail.com>
2020-05-22 11:25:33 -0400

    add schooling years table

--- etl/enoe/enoe_schooling_years.py
+++ etl/enoe/enoe_schooling_years.py
@@ -9,25 +9,28 @@ from bamboo_lib.steps import LoadStep
 
 class ExtractStep(PipelineStep):
     def run_step(self, prev, params):
-        df_labels = pd.ExcelFile("https://storage.googleapis.com/datamexico-data/shared/age_range.xlsx")
-        df = pd.read_excel(df_labels, "age_range")
+        df = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSKwGL6bkv7hJGc5szpkXmAV1rhRmkpN6i5W7-LdyUtpNntUtHJDzFDkiqX-AfhzBFaF2t5jB5VdsBo/pub?output=csv")
         return df
 
-class DimAgeRangePipeline(EasyPipeline):
+class DimSchoolingYearsPipeline(EasyPipeline):
     @staticmethod
     def steps(params):
         db_connector = Connector.fetch("clickhouse-database", open("../conns.yaml"))
         dtype = {
-            "age":           "UInt8",
-            "name_es":       "String",
-            "name_en":       "String",
-            "age_range_id":  "UInt8"
+            "schooling_years":           "UInt8",
+            "name_es":                   "String",
+            "name_en":                   "String",
+            "schooling_years_range_id":  "UInt8"
         }
 
         extract_step = ExtractStep()
         load_step = LoadStep(
-            "dim_shared_age", db_connector, if_exists="drop", dtype=dtype,
-            pk=["age"]
+            "dim_shared_enoe_schooling_years", db_connector, if_exists="drop", dtype=dtype,
+            pk=["schooling_years"]
         )
 
         return [extract_step, load_step]
+
+if __name__ == '__main__':
+    pp = DimSchoolingYearsPipeline()
+    pp.run({})
