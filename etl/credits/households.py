0c4ba63a4cbccb7160bb325064454440c0bbf4e1
D2SD <samueloso21@gmail.com>
2020-07-27 14:56:47 -0400

    households credits, credits refactor

--- etl/credits/households.py
+++ etl/credits/households.py
@@ -3,7 +3,7 @@ import pandas as pd
 from bamboo_lib.connectors.models import Connector
 from bamboo_lib.models import Parameter, EasyPipeline, PipelineStep
 from bamboo_lib.steps import DownloadStep, LoadStep
-from shared import COLUMNS, AGE_RANGE, COMPANY_SIZE, PERSON_TYPE, SEX, MISSING_MUN, replace_geo, norm, ReadStep
+from shared import COLUMNS, AGE_RANGE, PERSON_TYPE, SEX, MISSING_MUN, replace_geo, norm, ReadStep
 
 class TransformStep(PipelineStep):
     def run_step(self, prev, params):
@@ -15,8 +15,8 @@ class TransformStep(PipelineStep):
         # replace members in dimensions
         df['sex'].replace(SEX, inplace=True)
         df['person_type'].replace(PERSON_TYPE, inplace=True)
-        df['company_size'].replace(COMPANY_SIZE, inplace=True)
         df['age_range'].replace(AGE_RANGE, inplace=True)
+        df.drop(columns=['company_size'], inplace=True)
 
         for col in ['ent_id', 'mun_id']:
             df[col] = df[col].apply(lambda x: norm(x)).str.upper()
@@ -36,14 +36,14 @@ class TransformStep(PipelineStep):
             df.loc[~df['mun_id'].isin(list(mun.values())), 'ent_id'].astype(str) + '999'
         df.loc[df['level'] == 'Municipality', 'ent_id'] = '0'
 
-        df = df[['ent_id', 'mun_id', 'level', 'sex', 'person_type', 'company_size', 'age_range', 'count']].copy()
+        df = df[['ent_id', 'mun_id', 'level', 'sex', 'person_type', 'age_range', 'count']].copy()
 
         for col in df.columns[df.columns != 'level']:
             df[col] = df[col].astype(int)
 
         return df
 
-class IMSSCreditsPipeline(EasyPipeline):
+class HouseholdsPipeline(EasyPipeline):
     @staticmethod
     def steps(params):
         db_connector = Connector.fetch('clickhouse-database', open('../conns.yaml'))
@@ -54,13 +54,12 @@ class IMSSCreditsPipeline(EasyPipeline):
             'level':        'String', 
             'sex':          'UInt8', 
             'person_type':  'UInt8', 
-            'company_size': 'UInt8', 
             'age_range':    'UInt8', 
             'count':        'UInt32'
         }
 
         download_step = DownloadStep(
-            connector=['imss-ent-total', 'imss-mun-total'],
+            connector=['households-ent-total', 'households-mun-total'],
             connector_path='conns.yaml'
         )
 
@@ -68,12 +67,12 @@ class IMSSCreditsPipeline(EasyPipeline):
         transform_step = TransformStep()
 
         load_step = LoadStep(
-            'imss_credits', db_connector, dtype=dtype, if_exists='drop',
+            'households_credits', db_connector, dtype=dtype, if_exists='drop',
             pk=['ent_id', 'mun_id', 'level']
         )
 
         return [download_step, read_step, transform_step, load_step]
 
 if __name__ == "__main__":
-    pp = IMSSCreditsPipeline()
+    pp = HouseholdsPipeline()
     pp.run({})
--- etl/credits/shared.py
+++ etl/credits/shared.py
@@ -1,6 +1,8 @@
 
 import unicodedata
+import pandas as pd
 from bamboo_lib.helpers import query_to_df
+from bamboo_lib.models import PipelineStep
 from bamboo_lib.connectors.models import Connector
 
 COLUMNS = {
@@ -48,19 +50,48 @@ SEX = {
     'NO_IDENTIFICADO': 0
 }
 
+MISSING_MUN = {
+    'SILAO': 'SILAO DE LA VICTORIA',
+    'SAN PEDRO MIXTEPEC -DTO. 22 -': 20318,
+    'MEDELLIN': 'MEDELLIN DE BRAVO',
+    'HEROICA CIUDAD DE JUCHITAN DE ZARAGOZA': 'JUCHITAN DE ZARAGOZA',
+    'GRAL. ESCOBEDO': 19021,
+    'TLAQUEPAQUE': 'SAN PEDRO TLAQUEPAQUE',
+    'TLALTIZAPAN': 'TLALTIZAPAN DE ZAPATA'
+}
+
 
 def norm(string):
     return unicodedata.normalize('NFKD', string).encode('ASCII', 'ignore').decode("latin-1")
 
 def replace_geo():
+    """ query for ent, mun geo dimension"""
     db_connector = Connector.fetch('clickhouse-database', open('../conns.yaml'))
+
     ent = query_to_df(db_connector, 'select ent_id, ent_name from dim_shared_geography_ent')
     ent['ent_name'] = ent['ent_name'].apply(lambda x: norm(x)).str.upper()
     ent = dict(zip(ent['ent_name'], ent['ent_id']))
 
-    db_connector = Connector.fetch('clickhouse-database', open('../conns.yaml'))
     mun = query_to_df(db_connector, 'select mun_id, mun_name from dim_shared_geography_mun')
     mun['mun_name'] = mun['mun_name'].apply(lambda x: norm(x)).str.upper()
     mun = dict(zip(mun['mun_name'], mun['mun_id']))
 
     return [ent, mun] 
+
+class ReadStep(PipelineStep):
+    def run_step(self, prev, params):
+        df = pd.read_csv(prev[0], encoding='latin-1')
+        df.columns = df.columns.str.lower()
+        df.rename(columns=COLUMNS, inplace=True)
+        df['mun_id'] = '0'
+        df['level'] = 'State'
+        df_ent = df.copy()
+
+        df = pd.read_csv(prev[1], encoding='latin-1')
+        df.columns = df.columns.str.lower()
+        df.rename(columns=COLUMNS, inplace=True)
+        df['level'] = 'Municipality'
+
+        df = df.append(df_ent, sort=False)
+
+        return df
