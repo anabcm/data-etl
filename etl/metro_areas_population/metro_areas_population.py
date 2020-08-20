6ed430d02fae6d5af852625c106c75b5c3b3f89a
EduardoVasquez <eduardovasquez42@gmail.com>
2020-08-20 12:06:15 -0400

    adding metro areas pipeline + data source

---
+++ etl/metro_areas_population/metro_areas_population.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from bamboo_lib.connectors.models import Connector
+from bamboo_lib.models import EasyPipeline
+from bamboo_lib.models import Parameter
+from bamboo_lib.models import PipelineStep
+from bamboo_lib.steps import DownloadStep
+from bamboo_lib.steps import LoadStep
+
+
+class TransformStep(PipelineStep):
+    def run_step(self, prev, params):
+
+        df = pd.read_csv(prev, header=0)
+
+        # Replacing "no available" values with 0's
+        df.replace({"n.a." : 0}, inplace=True)
+
+        # Melt step to get population by year 
+        df = pd.melt(df, id_vars = ["zona_metropolitana", "zona_metropolitana_id", "mun", "mun_id"], 
+             value_vars = ["2000", "2010", "2015"])
+
+        # Renaming columns to usable names
+        df.rename(columns={'variable': 'year', 'value': 'population'}, inplace=True)
+
+        # Transforming str columns into int values
+        df["zona_metropolitana_id"] = df["zona_metropolitana_id"].astype(int)
+        df["mun_id"] = df["mun_id"].astype(int)
+        df["year"] = df["year"].astype(int)
+        df["population"] = df["population"].astype(int)
+
+        return df
+
+class MetroAreaPopulationPipeline(EasyPipeline):
+    @staticmethod
+    def parameter_list():
+        return [
+            Parameter(label="Index", name="index", dtype=str)
+        ]
+
+    @staticmethod
+    def steps(params):
+        db_connector = Connector.fetch("clickhouse-database", open("../conns.yaml"))
+
+        dtype = {
+            "zona_metropolitana_id":        "UInt32",
+            "mun_id":                       "UInt32",
+            "population":                   "UInt64",
+            "year":                         "UInt16"
+        }
+
+        download_step = DownloadStep(
+            connector="population-data",
+            connector_path="conns.yaml"
+        )
+        transform_step = TransformStep()
+        load_step = LoadStep(
+            "conapo_metro_area_population", db_connector, if_exists="append", pk=["zona_metropolitana_id", "mun_id"], dtype=dtype, 
+        )
+
+        return [download_step, transform_step, load_step]
