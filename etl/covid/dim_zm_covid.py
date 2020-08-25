bed4c7483da324793ada1b6ffb2c551e73917b33
D2SD <samueloso21@gmail.com>
2020-08-25 15:47:16 -0400

    dim zm covid refactor

--- etl/covid/dim_zm_covid.py
+++ etl/covid/dim_zm_covid.py
@@ -15,27 +15,27 @@ class TransformStep(PipelineStep):
         # Replacing "no available" values with 0's
         df.replace({"n.a." : 0}, inplace=True)
 
-        df = df[['mun', 'mun_id', 'zona_metropolitana', 'zona_metropolitana_id']].copy()
+        df = df[['zona_metropolitana', 'zona_metropolitana_id']].copy()
 
         # Transforming str columns into int values
         df["zona_metropolitana_id"] = df["zona_metropolitana_id"].astype(int)
-        df["mun_id"] = df["mun_id"].astype(int)
 
         df.rename(columns={
             'zona_metropolitana':    'zm_name',
             'zona_metropolitana_id': 'zm_id'
         }, inplace=True)
 
+        df.drop_duplicates(subset=['zm_id'], inplace=True)
+
         return df
 
-class MetroAreaPopulationPipeline(EasyPipeline):
+class MetroAreaCovid(EasyPipeline):
     @staticmethod
     def steps(params):
         db_connector = Connector.fetch("clickhouse-database", open("../conns.yaml"))
 
         dtype = {
-            "zm_id":   "UInt32",
-            "mun_id":  "UInt32",
+            "zm_id":   "UInt32"
         }
 
         download_step = DownloadStep(
@@ -46,11 +46,11 @@ class MetroAreaPopulationPipeline(EasyPipeline):
         transform_step = TransformStep()
 
         load_step = LoadStep(
-            "dim_conapo_metro_area_population", db_connector, if_exists="append", pk=["zm_id", "mun_id"], dtype=dtype, 
+            "dim_zm_covid", db_connector, if_exists="append", pk=["zm_id"], dtype=dtype, 
         )
 
         return [download_step, transform_step, load_step]
 
 if __name__ == "__main__":
-    pp = MetroAreaPopulationPipeline()
+    pp = MetroAreaCovid()
     pp.run({})
