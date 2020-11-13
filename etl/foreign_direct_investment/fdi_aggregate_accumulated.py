6ab57b0cee935c6d144288de4d7706f0a456db8c
D2SD <samueloso21@gmail.com>
2020-11-13 15:36:07 -0300

    add nation level

--- etl/foreign_direct_investment/fdi_aggregate_accumulated.py
+++ etl/foreign_direct_investment/fdi_aggregate_accumulated.py
@@ -27,8 +27,6 @@ class TransformStep(PipelineStep):
         data = prev
 
         result = {}
-        #historic = pd.DataFrame()
-        #last_period = pd.DataFrame()
         historic = {}
         last_period = {}
 
@@ -39,6 +37,7 @@ class TransformStep(PipelineStep):
                 'Año': 'year',
                 'Entidad federativa': 'ent_id',
                 'País de Origen DEAE': 'country_id',
+                'País': 'country_id',
                 'Subsector': 'subsector_id',
                 'Sector': 'sector_id',
                 'Rama': 'industry_group_id',
@@ -47,27 +46,9 @@ class TransformStep(PipelineStep):
                 'Monto C': 'value_c'
             }, inplace=True)
 
-            pk_id = [x for x in df.columns if x in ['sector_id', 'subsector_id', 'industry_group_id']][0]
+            pk_id = [x for x in df.columns if x in ['country_id', 'sector_id', 'subsector_id', 'industry_group_id']][0]
 
             df.dropna(subset=[pk_id], inplace=True)
-
-            # get end_id dimension
-            #dim_geo, dim_country = get_dimensions()
-
-            """if params.get('level') == 'ent_id':
-                df['ent_id'].replace(dict(zip(dim_geo['ent_name'], dim_geo['ent_id'])), inplace=True)
-
-            else:
-                df['country_id'].replace(COUNTRY_REPLACE, inplace=True)
-                df['country_id'].replace(dict(zip(dim_country['country_name_es'], dim_country['iso3'])), inplace=True)"""
-
-            split = df[pk_id].str.split(' ', n=1, expand=True)
-            df[pk_id] = split[0]
-            df[pk_id] = df[pk_id].astype(int)
-
-            if pk_id == 'sector_id':
-                df['sector_id'].replace(SECTOR_REPLACE, inplace=True)
-                df['sector_id'] = df['sector_id'].astype(str)
 
             temp = pd.DataFrame()
             top_3_historic = pd.DataFrame()
@@ -77,35 +58,42 @@ class TransformStep(PipelineStep):
                 # top 3 entidades federativas que acumulan mas IED 1999 - 2020
                 temp = df.loc[df[pk_id] == ele, ['year', pk_id, 'value', 'count']].groupby(by=[pk_id]).sum().reset_index()
                 temp = temp.sort_values(by=['value'], ascending=False)
-                #temp = temp.sort_values(by=['value'], ascending=False)[:3]
-                #temp['top'] = range(1, temp.shape[0] + 1)
-                # temp['indicador'] = 1
-                ## fill levels to append
-                """temp = fill_levels(temp, pk_id)
-                temp['year'] = 0"""
+                temp['check'] = None
                 for item in temp.iterrows():
-                    temp.loc[temp[pk_id] == item[1][pk_id], 'value'] = \
+                    temp.loc[temp[pk_id] == item[1][pk_id], 'check'] = \
                         check_confidentiality_no_geo(df, ele, pk_id, 'value_c', 'C',  item[1]['value'])
                 top_3_historic = top_3_historic.append(temp, sort=False)
 
                 # top 3 entidades federativas que acumulan mas IED ultimo anio
                 temp = df.loc[df[pk_id] == ele, ['year', pk_id, 'value', 'count']].groupby(by=['year', pk_id]).sum().reset_index()
                 temp = temp.loc[temp['year'] == temp['year'].max()].sort_values(by=['value'], ascending=False)
-                #temp = temp.loc[temp['year'] == temp['year'].max()].sort_values(by=['value'], ascending=False)[:3]
-                #temp['top'] = range(1, temp.shape[0] + 1)
-                # temp['indicador'] = 2
+                temp['check'] = None
                 for item in temp.iterrows():
-                    temp.loc[(temp['year'] == item[1]['year']) & (temp[pk_id] == item[1][pk_id]), 'value'] = \
+                    temp.loc[(temp['year'] == item[1]['year']) & (temp[pk_id] == item[1][pk_id]), 'check'] = \
                         list(df.loc[(df['year'] == item[1]['year']) & (df[pk_id] == item[1][pk_id]), 'value_c'])[0]
-                ## fill levels to append
-                #temp = fill_levels(temp, pk_id)
                 top_3_last_period = top_3_last_period.append(temp, sort=False)
+
+            if pk_id == 'country_id':
+                top_3_historic.sort_values(by=['value'], ascending=False, inplace=True)
+                top_3_historic.drop(columns=['value'], inplace=True)
+                top_3_historic.rename(columns={'check': 'value'}, inplace=True)
+                top_3_historic = top_3_historic[:3]
+                top_3_last_period.sort_values(by=['value'], ascending=False, inplace=True)
+                top_3_last_period.drop(columns=['value'], inplace=True)
+                top_3_last_period.rename(columns={'check': 'value'}, inplace=True)
+                top_3_last_period = top_3_last_period[:3]
+                top_3_historic['top'] = range(1, top_3_historic.shape[0] + 1)
+                top_3_last_period['top'] = range(1, top_3_last_period.shape[0] + 1)
 
             historic[pk_id.split('_id')[0]] = top_3_historic.to_dict(orient='records')
             last_period[pk_id.split('_id')[0]] = top_3_last_period.to_dict(orient='records')
 
-        result['accumulated_historic_IED'] = historic
-        result['accumulated_last_period_IED'] = last_period
+        if pk_id == 'country_id':
+            result['nation_accumulated_historic_IED'] = historic
+            result['nation_accumulated_last_period_IED'] = last_period
+        else:
+            result['accumulated_historic_IED'] = historic
+            result['accumulated_last_period_IED'] = last_period
 
         with open('{}.json'.format(params.get('file_name')), 'w') as outfile:
             json.dump(result, outfile)
@@ -117,28 +105,15 @@ class FDIaggregatePipeline(EasyPipeline):
     def parameter_list():
         return [
             Parameter(name='sheets', dtype=list),
-            Parameter(name='file_name', dtype=str)
+            Parameter(name='file_name', dtype=str),
+            Parameter(name='source', dtype=str)
         ]
 
     @staticmethod
     def steps(params):
-        #db_connector = Connector.fetch("clickhouse-database", open("../conns.yaml"))
-
-        """dtype = {
-            'year':              'UInt16',
-            'ent_id':            'UInt8',
-            'country_id':        'String',
-            'sector_id':         'String',
-            'subsector_id':      'UInt16',
-            'industry_group_id': 'UInt16',
-            'top':               'UInt8',
-            'indicador':         'UInt8',
-            'value':             'Float32',
-            'count':             'UInt16'
-        }"""
 
         download_step = DownloadStep(
-            connector="fdi-data-additional-3",
+            connector=params.get('source'),
             connector_path="conns.yaml"
         )
 
@@ -149,8 +124,10 @@ class FDIaggregatePipeline(EasyPipeline):
 
 if __name__ == "__main__":
     pp = FDIaggregatePipeline()
-    for file_name, sheets in {'accumulated_value': ['1', '2', '3']}.items():
+    for file_name, sheets in {'accumulated_value': [['1', '2', '3'], 'fdi-data-additional-3'],
+                              'top3_country_nation': [['10.1'], 'fdi-data']}.items():
         df = pp.run({
-            'sheets': sheets,
+            'source': sheets[1],
+            'sheets': sheets[0],
             'file_name': file_name
         })
