c19f05e2c8334d8fef337d0a7ad2360c5ff48670
D2SD <samueloso21@gmail.com>
2020-11-17 17:09:25 -0300

    add eng names fdi countries

--- etl/foreign_direct_investment/shared.py
+++ etl/foreign_direct_investment/shared.py
@@ -8,7 +8,7 @@ def get_dimensions():
     dim_geo_query = 'SELECT ent_name, ent_id FROM dim_shared_geography_ent'
     dim_geo = query_to_df(db_connector, raw_query=dim_geo_query)
 
-    dim_country_query = 'SELECT country_name_es, iso3 FROM dim_shared_country'
+    dim_country_query = 'SELECT country_name_es, country_name, iso3 FROM dim_shared_country'
     dim_country = query_to_df(db_connector, raw_query=dim_country_query)
 
     return [dim_geo, dim_country]
