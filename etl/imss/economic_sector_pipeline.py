702549902d0e44110aeabecdac544598776456a9
D2SD <samueloso21@gmail.com>
2020-11-30 17:34:36 -0300

    imss economic sector refactor

--- etl/imss/economic_sector_pipeline.py
+++ etl/imss/economic_sector_pipeline.py
@@ -9,20 +9,7 @@ from bamboo_lib.steps import LoadStep
 class ExtractStep(PipelineStep):
     def run_step(self, prev, params):
 
-        # Data Dictonary from IMSS
-        xls = pd.ExcelFile(prev)
-
-        # Economic sector 1 ,2 and 4 Sheets from Data dictonary from IMSS
-        df1 = pd.read_excel(xls, "sector 1", header=1)
-        df2 = pd.read_excel(xls, "sector 2", header=1)
-        df3 = pd.read_excel(xls, "sector 4", header=1)
-        df = pd.merge(df1, df2, on=["sector_economico_1"])
-        df = pd.merge(df, df3, on=["sector_economico_1", "sector_economico_2_2pos"])
-        df = df.drop(columns=["sector_economico_2_x", "sector_economico_2_y", "sector_economico_4"])
-        df = df.rename(columns={"sector_economico_1" : "level_1_id", "sector_economico_2_2pos" : "level_2_id", "sector_economico_4_4_pos" : 
-                                "level_4_id", "descripción sector_economico_1": "level_1_name", "descripción sector_economico_2" : "level_2_name", 
-                                "descripción sector_economico_4" : "level_4_name"})
-        df["level_2_id"] = df["level_2_id"].astype(int)
+        df = pd.read_excel(prev)
 
         return df
 
@@ -31,16 +18,19 @@ class DimEconomicSectorPipeline(EasyPipeline):
     def steps(params):
         db_connector = Connector.fetch("clickhouse-database", open("../conns.yaml"))
         dtype = {
-            "level_1_id":   "UInt8", 
-            "level_1_name": "String", 
-            "level_2_id":   "UInt8", 
-            "level_2_name": "String",
-            "level_4_id":   "UInt16", 
-            "level_4_name": "String"
+            "level_1_id":   "UInt8",
+            "level_1_name_es": "String",
+            "level_1_name_en": "String",
+            "level_2_id":   "UInt8",
+            "level_2_name_es": "String",
+            "level_2_name_en": "String",
+            "level_4_id":   "UInt16",
+            "level_4_name_es": "String",
+            "level_4_name_en": "String"
         }
 
         download_step = DownloadStep(
-            connector="imss-dimension",
+            connector="imss-economic-sector-dimension",
             connector_path="conns.yaml"
         )
 
