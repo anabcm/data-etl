36f48981ba798f4757abc9a4a7bc1f5933b1ef9f
D2SD <samueloso21@gmail.com>
2021-02-02 14:31:40 -0300

    covid refactot

--- etl/covid/covid_pipeline.py
+++ etl/covid/covid_pipeline.py
@@ -7,7 +7,7 @@ from bamboo_lib.helpers import grab_parent_dir
 from bamboo_lib.connectors.models import Connector
 from bamboo_lib.models import EasyPipeline, PipelineStep, Parameter
 from bamboo_lib.steps import DownloadStep, LoadStep, UnzipToFolderStep
-from shared import rename_columns, rename_countries, values_check, NoUpdateException
+from shared import rename_columns, RENAME_COUNTRIES, values_check, NoUpdateException
 from etl.helpers import norm
 
 
@@ -16,12 +16,16 @@ class TransformStep(PipelineStep):
 
         data = sorted(glob.glob('*.csv'))
 
+        COLS_SUBSET = list(rename_columns.keys()) + ['municipio_res', 'entidad_res', 'id_registro', 'clasificacion_final']
+
         if params.get('file_path'):
             df = pd.read_csv(params.get('file_path'), encoding='latin-1')
         else:
             df = pd.read_csv(data[-1], encoding='latin-1')
 
         df.columns = [x.strip().lower().replace(' ', '_') for x in df.columns]
+
+        df = df[COLS_SUBSET].copy()
 
         df['entidad_res'] = df['entidad_res'].astype(str).str.zfill(2)
         df['municipio_res'] = df['municipio_res'].astype(str).str.zfill(3)
@@ -37,24 +41,34 @@ class TransformStep(PipelineStep):
         df['is_dead'] = 1
         df.loc[df['death_date'].isna(), 'is_dead'] = 0
 
+        df['country_nationality_old'] = df['country_nationality']
+        df['country_origin_old'] = df['country_origin']
+
         for col in ['country_origin', 'country_nationality']:
             df[col] = df[col].fillna('xxa')
             df[col] = df[col].str.strip().str.lower()
             df[col] = df[col].apply(lambda x: norm(x))
 
-        df['country_nationality'].replace(rename_countries, inplace=True)
-        df['country_origin'].replace(rename_countries, inplace=True)
+        df['country_nationality'] = df['country_nationality'].map(RENAME_COUNTRIES)
+        df['country_origin'] = df['country_origin'].map(RENAME_COUNTRIES)
+        df['country_origin'] = df['country_origin'].fillna('xxa')
+        df['country_nationality'] = df['country_nationality'].fillna('xxa')
+        # assert df['country_nationality'].isnull().sum() == 0, "Null Countries (Nationality)"
+        # assert df['country_origin'].isnull().sum() == 0, "Null Countries (Origin)"
 
         df['death_date'] = df['death_date'].astype(float)
 
-        for col in [x for x in df.columns if x not in ['country_nationality', 'country_origin', 'death_date']]:
-            df[col] = df[col].astype(int)
-
-        df['id'] = range(1, df.shape[0]+1)
+        df = df[['updated_date', 'origin', 'type_health_institution_attended', 'health_institution_attended_ent',
+                'sex', 'patient_origin_ent_id', 'patient_type', 'ingress_date', 'symptoms_date', 'intubated',
+                'pneumonia_diagnose', 'age', 'nationality', 'pregnancy', 'speaks_indigenous_language', 'diabetes_diagnose',
+                'COPD_diagnose', 'asthma_diagnose', 'inmunosupresion_diagnose', 'hypertension_diagnose',
+                'diagnosis_another_disease', 'cardiovascular_diagnose', 'obesity_diagnose', 'chronic_kidney_failure_diagnose',
+                'smoking_diagnose', 'contact_another_covid_case', 'covid_positive', 'migrant', 'required_ICU',
+                'patient_residence_mun_id', 'is_dead', 'clasificacion_final']].astype(int)
 
         df['sex'] = df['sex'].astype(str)
         df['sex'].replace({'1': 2,
-                           '2': 1}, inplace=True)
+                        '2': 1}, inplace=True)
 
         # replace unknown municipalities
         df.loc[df['patient_residence_mun_id'].isin([97997, 98998, 99999]), 'patient_residence_mun_id'] = 33000
@@ -63,6 +77,7 @@ class TransformStep(PipelineStep):
         df['time_id'] = df['updated_date']
 
         # ids refactor
+        df['covid_positive'] = None
         df.loc[df['clasificacion_final'].isin([1,2,3]), 'covid_positive'] = 1
         df.loc[df['clasificacion_final'].isin([4,5,6]), 'covid_positive'] = 3
         df.loc[df['clasificacion_final'] == 7, 'covid_positive'] = 2
