8cc99de8a3e98e00c020153948fa6f3a5745574f
D2SD <samueloso21@gmail.com>
2021-02-03 17:41:54 -0300

    add static file to inegi economic census

---
+++ etl/inegi_economic_census/static.py
@@ -0,0 +1,12 @@
+
+FILL_COLUMNS = ['ent_id', 'mun_id', 'sector_id', 'subsector_id', 'rama_id']
+
+CENSUS_2019_RENAME = ['ent_id', 'mun_id', 'sector_id', 'subsector_id', 'rama_id', 'denominacion', 'UE', 'total_personal_ocupado', 
+                      'total_personal_ocupado_dependiente', 'personal_ocupado_remunerado', 'propietarios_familiares_otros_no_remunerados', 
+                      'no_dependiente_razon_social', 'remuneraciones', 'gasto_consumo_bienes_servicios', 'ingreso_suministro_bienes_servicios', 
+                      'produccion_total_bruta', 'consumo_intermedio', 'valor_agregado_censal_bruto', 'formacion_bruta_capital_fijo', 
+                      'variacion_total_existencias', 'activos_fijos', 'depreciacion_activos_fijos', 'participacion_remuneraciones_gastos_consumo_bienes_servicios', 
+                      'valor_agregado_censal_bruto_produccion_bruta_total', 'tasa_rentabilidad_promedio', 'activos_fijos_produccion_bruta_total', 
+                      'participacion_depreciacion_valor_Activos_fijos', 'margen_bruto_operacion', 'participacion_consumo_intermedio_produccion_bruta_total', 
+                      'valor_agregado_total_activos_fijos', 'valor_agregado_promedio_persona_ocupada', 'produccion_bruta_total_personal_ocupado_total',
+                      'remuneracion_media_persona_remunerada', 'valor_activos_fijos_persona_ocupada']
