1a18c3f58b232bb7277175334236018eb8ce85f5
D2SD <samueloso21@gmail.com>
2021-02-11 10:49:21 -0300

    mun_id minor changes economic census features

--- etl/inegi_economic_census/economic_census_2019_main_features.py
+++ etl/inegi_economic_census/economic_census_2019_main_features.py
@@ -114,9 +114,7 @@ class MunSec(PipelineStep):
         df = df.loc[~df['sector_id'].isna()].copy()
         df = df.loc[~df['mun_id'].isna()].copy()
 
-        df['ent_id'] = df['ent_id'].str.split(' ', expand=True)[0].str.strip().astype(int)
         df['mun_id'] = df['mun_id'].str.split(' ', expand=True)[0].str.strip().astype(int)
-        df['mun_id'] = (df['ent_id'].astype(str) + df['mun_id'].astype(str).str.zfill(3)).astype(int)
 
         df.drop(columns=['ent_id', 'subsector_id', 'rama_id', 'denominacion'], inplace=True)
 
@@ -204,7 +202,15 @@ class EconomicCensusPipeline(EasyPipeline):
 
         load_step = LoadStep(
             'inegi_economic_census_main_features', db_connector, dtype=dtypes, if_exists='drop', 
-            pk=['mun_id', 'ent_id', 'year']
+            pk=['mun_id', 'ent_id', 'year'], nullable_list = ['total_personal_ocupado', 'total_personal_ocupado_dependiente', 
+                'personal_ocupado_remunerado', 'propietarios_familiares_otros_no_remunerados', 'no_dependiente_razon_social',
+                'remuneraciones', 'gasto_consumo_bienes_servicios', 'ingreso_suministro_bienes_servicios', 'produccion_total_bruta', 
+                'consumo_intermedio', 'valor_agregado_censal_bruto', 'formacion_bruta_capital_fijo', 'variacion_total_existencias', 
+                'activos_fijos', 'depreciacion_activos_fijos', 'participacion_remuneraciones_gastos_consumo_bienes_servicios',
+                'valor_agregado_censal_bruto_produccion_bruta_total', 'tasa_rentabilidad_promedio', 'activos_fijos_produccion_bruta_total', 
+                'participacion_depreciacion_valor_Activos_fijos', 'margen_bruto_operacion', 'participacion_consumo_intermedio_produccion_bruta_total', 
+                'valor_agregado_total_activos_fijos', 'valor_agregado_promedio_persona_ocupada', 'produccion_bruta_total_personal_ocupado_total',
+                'remuneracion_media_persona_remunerada', 'valor_activos_fijos_persona_ocupada']
         )
 
         return [download_step, read_step, ent_sector, ent_subsector, ent_rama, mun_Sec, join_step, load_step]
