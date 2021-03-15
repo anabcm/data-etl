5d97e658e73640f862a03f6f7aae2a016de9caaf
D2SD <samueloso21@gmail.com>
2021-03-15 15:27:09 -0300

    covid fix date dimension

--- etl/covid/util.py
+++ etl/covid/util.py
@@ -15,8 +15,8 @@ data = sorted(glob.glob('*.csv'))
 #    os.system('bamboo-cli --folder . --entry covid_pipeline --file_path="{}"'.format(file_path))
 
 db_connector = Connector.fetch('clickhouse-database', open('../conns.yaml'))
-min_query = 'SELECT min(ingress_date), min(symptoms_date), min(death_date), min(updated_date) FROM gobmx_covid'
-max_query = 'SELECT max(ingress_date), max(symptoms_date), max(death_date), max(updated_date) FROM gobmx_covid'
+min_query = 'SELECT min(ingress_date), min(symptoms_date), min(updated_date) FROM gobmx_covid'
+max_query = 'SELECT max(ingress_date), max(symptoms_date), max(updated_date) FROM gobmx_covid'
 min_value = str(min(query_to_df(db_connector, raw_query=min_query).iloc[0].to_list()))
 max_value = str(max(query_to_df(db_connector, raw_query=max_query).iloc[0].to_list()))
 min_time = '{}-{}-{}'.format(min_value[:4], min_value[4:6], min_value[6:])
