9f442b9309e11691b1af53c97b2998dc4ec3b970
D2SD <samueloso21@gmail.com>
2021-03-16 19:07:55 -0300

    add time dimension to covid pipeline

--- etl/covid/covid_pipeline.py
+++ etl/covid/covid_pipeline.py
@@ -10,6 +10,7 @@ from bamboo_lib.models import EasyPipeline, PipelineStep, Parameter
 from bamboo_lib.steps import DownloadStep, LoadStep, UnzipToFolderStep
 from shared import rename_columns, RENAME_COUNTRIES, values_check, NoUpdateException
 from bamboo_lib.helpers import query_to_df
+from dim_time_date import DimTimeDatePipeline
 from etl.helpers import norm
 
 def clean_tables(table_name: str):
@@ -180,3 +181,16 @@ if __name__ == '__main__':
             'chunk': chunk
         })
     print('Duration: {}'.format(datetime.now() - start_time))
+
+    # update time dimension
+    db_connector = Connector.fetch('clickhouse-database', open('../conns.yaml'))
+    min_query = 'SELECT min(ingress_date), min(symptoms_date), min(updated_date) FROM gobmx_covid'
+    max_query = 'SELECT max(ingress_date), max(symptoms_date), max(updated_date) FROM gobmx_covid'
+    min_value = str(min(query_to_df(db_connector, raw_query=min_query).iloc[0].to_list()))
+    max_value = str(max(query_to_df(db_connector, raw_query=max_query).iloc[0].to_list()))
+    min_time = '{}-{}-{}'.format(min_value[:4], min_value[4:6], min_value[6:])
+    max_time = '{}-{}-{}'.format(max_value[:4], max_value[4:6], max_value[6:])
+
+    pp = DimTimeDatePipeline()
+    pp.run({'init': min_time,
+            'end': max_time})
