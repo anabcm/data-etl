581276c4ec3e4756a56790fb16c1420b4b29698f
D2SD <samueloso21@gmail.com>
2021-03-17 17:30:03 -0300

    dim geo country refactor

--- etl/geo/dim_country_pipeline.py
+++ etl/geo/dim_country_pipeline.py
@@ -8,7 +8,7 @@ from bamboo_lib.steps import LoadStep
 
 class TransformStep(PipelineStep):
     def run_step(self, prev, params):
-        df = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQ_cYou4TSCU8zxH3S6Ze0HZM8Dv3bBUSXKYVN5UYMnkinSwjnAuSHr-T-mmLjALMFxyH0JduBECM6F/pub?output=csv")
+        df = pd.read_csv(prev)
         df["slug"] = df["iso3"]
         return df
 
@@ -16,11 +16,14 @@ class DimCountryGeographyPipeline(EasyPipeline):
     @staticmethod
     def steps(params):
         db_connector = Connector.fetch("clickhouse-database", open("../conns.yaml"))
-
+        download_step = DownloadStep(
+            connector='countries',
+            connector_path='conns.yaml'
+        )
         transform_step = TransformStep()
         load_step = LoadStep(
             "dim_shared_country", db_connector, if_exists="drop", nullable_list=["iso2"],
             pk=["iso3"]
         )
 
-        return [transform_step, load_step]
+        return [download_step, transform_step, load_step]
