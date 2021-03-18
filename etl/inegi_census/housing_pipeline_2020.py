2cf8d919843c4ae0c69584961e2ce8f5417c54b7
oportusgonzalo <gonzalo@datawheel.us>
2021-03-18 13:05:59 -0300

    pipe

--- etl/inegi_census/housing_pipeline_2020.py
+++ etl/inegi_census/housing_pipeline_2020.py
@@ -1,6 +1,5 @@
 import pandas as pd
-from bamboo_lib.models import PipelineStep
-from bamboo_lib.models import Parameter, EasyPipeline
+from bamboo_lib.models import Parameter, EasyPipeline, PipelineStep
 from bamboo_lib.connectors.models import Connector
 from bamboo_lib.steps import LoadStep, DownloadStep
 
@@ -8,11 +7,12 @@ class ReadStep(PipelineStep):
     def run(self, prev, params):
         df = pd.read_csv(prev)
         df.columns = df.columns.str.lower()
-        print(df)
         return df
 
 class TransformStep(PipelineStep):
     def run_step(self, prev, params):
+        df = prev
+        print(df)
 
         return df
 
@@ -24,21 +24,19 @@ class HousingPipeline(EasyPipeline):
         ]
 
     @staticmethod
-    def steps(params):
+    def steps(params, **kwargs):
         # Use of connectors specified in the conns.yaml file
         db_connector = Connector.fetch('clickhouse-database', open('../conns.yaml'))
 
-        dtype = {}
-
-        dl_step = DownloadStep(
-            connector='housing-data-2020',
-            connector_path='conns.yaml'
+        download_step = DownloadStep(
+            connector="housing-data-2020",
+            connector_path="conns.yaml"
         )
 
         read_step = ReadStep()
         transform_step = TransformStep()
         
-        return [dl_step, read_step]
+        return [download_step, read_step, transform_step]
 
 if __name__ == "__main__":
     pp = HousingPipeline()
@@ -46,4 +44,3 @@ if __name__ == "__main__":
         pp.run({
             "index": str(index).zfill(2)
             })
-        
