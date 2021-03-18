3376513b6ddfef930aabbe1727a1e14ae53f37da
D2SD <samueloso21@gmail.com>
2021-03-18 10:43:39 -0300

    ensu shared cities dimension

--- etl/shared/dim_shared_ensu_cities.py
+++ etl/shared/dim_shared_ensu_cities.py
@@ -2,12 +2,11 @@ import pandas as pd
 from bamboo_lib.models import PipelineStep
 from bamboo_lib.models import EasyPipeline
 from bamboo_lib.connectors.models import Connector
-from bamboo_lib.steps import LoadStep
+from bamboo_lib.steps import LoadStep, DownloadStep
 
 class ReadStep(PipelineStep):
     def run_step(self, prev, params):
-        url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQSnOLOLTXeKjlrLM-SRqEKC0v4ATLeAIEwYA4clBWmu3klCRnkcREwJrCVvCO2WDPzzpTPP1Ocj-H0/pub?output=xlsx"
-        df = pd.read_excel(url, sheet_name="dim_city", encoding="latin-1")
+        df = pd.read_excel(prev, sheet_name="dim_city", encoding="latin-1")
         return df
 
 class CoveragePipeline(EasyPipeline):
@@ -29,7 +28,11 @@ class CoveragePipeline(EasyPipeline):
         }
 
         # Definition ofreference_city_ each step
+        download_step = DownloadStep(
+            connector="ensu-shared-cities",
+            connector_path="conns.yaml"
+        )
         read_step = ReadStep()
         load_step = LoadStep("dim_shared_ensu_cities", db_connector, if_exists="drop", pk=["reference_city_id"], dtype=dtype)
 
-        return [read_step, load_step]
+        return [download_step, read_step, load_step]
