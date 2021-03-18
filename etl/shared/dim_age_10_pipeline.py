74cc148822c843c1ee3447de72030befcf929255
D2SD <samueloso21@gmail.com>
2021-03-18 15:10:51 -0300

    age range 10 refactor

--- etl/shared/dim_age_10_pipeline.py
+++ etl/shared/dim_age_10_pipeline.py
@@ -9,7 +9,7 @@ from bamboo_lib.steps import LoadStep
 
 class ExtractStep(PipelineStep):
     def run_step(self, prev, params):
-        df = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vRb3WBn3hfIBCke_rHUpvNECsTP4_SXYdnF66j9oRdIIjHgok5KSbQaI-uJOL7aBjrJ0TZ0aIcqLnBA/pub?output=csv')
+        df = pd.read_csv(prev)
         return df
 
 class DimSexPipeline(EasyPipeline):
@@ -23,10 +23,14 @@ class DimSexPipeline(EasyPipeline):
             "age_range_id":  "UInt8"
         }
 
+        download_step = DownloadStep(
+            connector="shared-age-10",
+            connector_path="conns.yaml"
+        )
         extract_step = ExtractStep()
         load_step = LoadStep(
             "dim_shared_age_10", db_connector, if_exists="drop", dtype=dtype,
             pk=["age"]
         )
 
-        return [extract_step, load_step]
+        return [download_step, extract_step, load_step]
