8aa5ada2b88c6fcc82745b9fa3e6a3a003ab83e6
mcastillomolina <micky.abreu@gmail.com>
2021-03-25 18:34:05 -0300

    refactor: Add's 2020 to population pipes

--- etl/inegi_census/population_ingest_2010.sh
+++ etl/inegi_census/population_ingest_2010.sh
@@ -1,4 +1,9 @@
-for i in $(seq -f "%02g" 1 32)
+#!/bin/bash
+
+for i in $(seq -f "%02g" 1 32); 
 do
-    bamboo-cli --folder . --entry population_pipeline_2010 --index="$i"
-done
+for j in "2010" "2020";
+do
+    bamboo-cli --folder . --entry population_pipeline_2010 --index="$i" --year="$j"
+done;
+done;
