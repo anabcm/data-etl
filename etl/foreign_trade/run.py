3ec2b288c92e0fafd89da239551e11cd6d025a87
oportusgonzalo <71090858+oportusgonzalo@users.noreply.github.com>
2021-04-14 18:06:44 -0400

    Update run.py

--- etl/foreign_trade/run.py
+++ etl/foreign_trade/run.py
@@ -2,14 +2,17 @@ import os
 from google.cloud import storage
 from util import get_level, check_update, LEVELS
 
+# Creates connection to Google Cloud
 storage_client = storage.Client.from_service_account_json(os.environ.get('GOOGLE_APPLICATION_CREDENTIALS'))
 bucket = storage_client.get_bucket('datamexico-data')
 blobs = bucket.list_blobs(prefix='foreign_trade')
+# Reads all files in foreign trade folder and returns it's path (EJ: /foreign_trade/Municipal/HS_4D/Annual/4D_mun_annual06.csv)
 files = [x.name for x in blobs]
 
 mun = []
 ent = []
 nat = []
+# For each file in the list, we append it's name to an array depending on the level of depth in it's geographical classification (Municipal, State, or National).
 for file in files:
 	val = ('https://storage.googleapis.com/datamexico-data/' + str(file)).split('/foreign_trade/')[1]
 	if 'Municipal' in val and '.csv' in val:
@@ -21,6 +24,9 @@ for file in files:
 
 print('nat files: {}, mun files: {}, ent files: {}'.format(len(nat), len(mun), len(ent)))
 
+# For each table (names defined for cubes), we check if there are new updates comparing with files already ingested in clickhouse database.
+# This can be done by the "check_update" function, which returns just new files stored in GCP.
+# We run the script "foreign_trade_pipeline" with the specific parameters.
 for table in ['economy_foreign_trade_', 'economy_foreign_trade_unanonymized_']:
 	nat_ = check_update(nat, table)
 	for url in nat_:
