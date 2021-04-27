e1ddc394011e248644bf11955ab0c20d447adcc4
mcastillomolina <micky.abreu@gmail.com>
2021-04-27 18:02:49 -0400

    adds new dimensions pipes

---
+++ etl/inegi_intercensal_survey/dim_inegi_migration_cause.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from bamboo_lib.connectors.models import Connector
+from bamboo_lib.models import EasyPipeline
+from bamboo_lib.models import Parameter
+from bamboo_lib.models import PipelineStep
+from bamboo_lib.steps import DownloadStep
+from bamboo_lib.steps import LoadStep
+
+
+class ExtractStep(PipelineStep):
+    def run_step(self, prev, params):
+        df_labels = pd.ExcelFile(prev)
+        df = pd.read_excel(df_labels, 'causa_mig')
+        df = df.drop(columns=['prev_id'])
+        return df
+
+
+class DimMigrationCausePipeline(EasyPipeline):
+    @staticmethod
+    def steps(params):
+        db_connector = Connector.fetch('clickhouse-database', open("../conns.yaml"))
+
+        dtype = {
+            'id':       'UInt8',
+            'name_es':  'String',
+            'name_en':  'String',
+        }
+        download_step = DownloadStep(
+            connector='labels-2',
+            connector_path='conns.yaml'
+        )
+        extract_step = ExtractStep()
+        load_step = LoadStep(
+            "dim_inegi_migration_cause", db_connector, if_exists="drop", dtype=dtype,
+            pk=['id']
+        )
+
+        return [download_step, extract_step, load_step]
