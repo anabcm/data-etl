aabad06eb749c47fd3f378802d5c411a5c76b4e3
Annie <annabaellaizm@gmail.com>
2021-04-27 11:04:42 -0400

    New Industrial Parks Pipeline

---
+++ etl/industrial_parks/dim_park_types.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+from bamboo_lib.connectors.models import Connector
+from bamboo_lib.models import Parameter, EasyPipeline, PipelineStep
+from bamboo_lib.steps import DownloadStep, LoadStep
+
+
+class TransformStep(PipelineStep):
+    def run_step(self, prev, params):
+
+        df = pd.read_csv(prev, sep=";", encoding="utf-8")
+        df = df[["park_type_id", "type_park_es", "type_park_en"]]
+        return df
+
+
+class DimParkTypes(EasyPipeline):
+    @staticmethod
+    def steps(params):
+        db_connector = Connector.fetch("clickhouse-database", open("../conns.yaml"))
+
+        dtype = {
+            "park_type_id":     "UInt8",
+            "type_park_es":     "String",
+            "type_park_en":     "String",
+        }
+
+        download_step = DownloadStep(
+            connector="types_industrial_parks",
+            connector_path="conns.yaml"
+        )
+
+        transform_step = TransformStep()
+
+        load_step = LoadStep(
+            "types_industrial_parks", db_connector, dtype=dtype,
+            if_exists="append", pk=["park_type_id"]
+        )
+
+        return [download_step, transform_step, load_step]
+
+
+if __name__ == "__main__":
+    dim_park_types_pipeline = DimParkTypes()
+    dim_park_types_pipeline.run({})
---
+++ etl/industrial_parks/industrial_park.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import numpy as np
+from bamboo_lib.connectors.models import Connector
+from bamboo_lib.models import EasyPipeline, PipelineStep, Parameter
+from bamboo_lib.steps import DownloadStep, LoadStep
+
+
+class TransformStep(PipelineStep):
+    def run_step(self, prev, params):
+        industrial_parks = pd.read_excel(prev[0])
+        dim_names = pd.read_csv(prev[1], sep=";", encoding="utf-8").to_dict(orient="list")
+
+        #Create dictionary for park names
+        dicto_names = dict(zip(dim_names["park_name"], dim_names["park_id"]))
+
+        df = industrial_parks
+        df = df[["CVEGEO", "NOMBRE", "MASTER_TIPO_DE_ASENTAMIENTO"]].copy()      
+        df = df.drop_duplicates()       
+
+        park_type = {
+            "Parque industrial": 1,
+            "Zona industrial": 2,
+            "Parque industrial en construcci�n/desarrollo": 3,
+            "Terreno industrial": 4,
+            "Edificio industrial": 5,
+            "Microparque": 6,
+            "Nave industrial": 7,
+            "Pol�gono industrial": 8,
+            "Cl�ster": 9,
+            "Campus universitario": 10,
+            "Lote industrial": 11
+        }
+
+        df["MASTER_TIPO_DE_ASENTAMIENTO"] = df["MASTER_TIPO_DE_ASENTAMIENTO"].replace(park_type)
+
+        df["NOMBRE"] = df["NOMBRE"].replace(dicto_names)
+        df.columns = ["mun_id", "park_name_id", "park_type_id"]
+
+        df["count_parks"] = 1
+        print(df.head())
+        return df
+
+class IndustrialParkPipeline(EasyPipeline):
+    @staticmethod
+    def steps(params):
+        db_connector = Connector.fetch(
+            "clickhouse-database", open("../conns.yaml"))
+
+        dtypes = {
+            "mun_id":           "UInt16",
+            "park_name_id":     "UInt16",
+            "park_type_id":     "UInt8",
+            "count_parks":      "UInt8"
+        }
+
+        download_step = DownloadStep(
+            connector=["dataset1_industrial_parks", "names_industrial_paks"],
+            connector_path="conns.yaml"
+        )
+
+        transform_step = TransformStep()
+        
+        load_step = LoadStep(
+            "industrial_parks", db_connector, if_exists="drop",
+            pk=["mun_id", "park_name_id", "park_type_id"], 
+            dtype=dtypes
+        )
+
+        return [download_step, transform_step, load_step]
+
+if __name__ == "__main__":
+    industrial_parks = IndustrialParkPipeline()
+    industrial_parks.run({})
