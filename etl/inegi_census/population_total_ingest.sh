5418aa4d98ac2718c284dfc0af8de3e030180108
oportusgonzalo <gonzalo@datawheel.us>
2021-04-29 11:21:07 -0400

    renamed pipes for population total

--- etl/inegi_census/population_total_ingest.sh
+++ etl/inegi_census/population_total_ingest.sh
@@ -4,6 +4,6 @@ for i in $(seq -f "%02g" 1 32);
 do
 for j in "2010" "2020";
 do
-    bamboo-cli --folder . --entry population_pipeline_2010 --index="$i" --year="$j"
+    bamboo-cli --folder . --entry population_total_pipeline --index="$i" --year="$j"
 done;
 done;
