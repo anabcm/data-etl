9c3a0624096894b244b1885899cb7717470e6008
mcastillomolina <micky.abreu@gmail.com>
2021-05-05 12:16:33 -0400

    little refactor to dimensions ingest in inegi_intercensal_survey

--- etl/inegi_intercensal_survey/dim_inegi_imperiment_cause.py
+++ etl/inegi_intercensal_survey/dim_inegi_imperiment_cause.py
@@ -9,7 +9,7 @@ class ExtractStep(PipelineStep):
     def run_step(self, prev, params):
         data = {'id':[1, 2, 3, 4, 5, 9], 
                 'name_en':['Because he was born that way', 'Because of a disease', 'Because of an accident', 'Because of advanced age', 'Because of another cause', 'Not Specified'],
-                'name_es':['Porque nacio asi', 'Por una enfermedad', 'Por un accidente', 'Por edad avanzada', 'Por otra causa', 'No Especificado']}
+                'name_es':['Porque nació asi', 'Por una enfermedad', 'Por un accidente', 'Por edad avanzada', 'Por otra causa', 'No Especificado']}
         df = pd.DataFrame(data=data)
 
         return df
--- etl/inegi_intercensal_survey/dim_ingest.sh
+++ etl/inegi_intercensal_survey/dim_ingest.sh
@@ -1,1 +1,7 @@
-bamboo-cli --folder etl/inegi_intercensal_census --entry dim_sex_pipeline
+#bamboo-cli --folder etl/inegi_intercensal_census --entry dim_sex_pipeline
+bamboo-cli --folder etl/inegi_intercensal_census --entry dim_inegi_entity_country_nation
+bamboo-cli --folder etl/inegi_intercensal_census --entry dim_inegi_imperiment_cause
+bamboo-cli --folder etl/inegi_intercensal_census --entry dim_inegi_imperiment
+bamboo-cli --folder etl/inegi_intercensal_census --entry dim_inegi_migration_cause
+bamboo-cli --folder etl/inegi_intercensal_census --entry dim_inegi_qdialect
+bamboo-cli --folder etl/inegi_intercensal_census --entry dim_inegi_religion
